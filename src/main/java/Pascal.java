945d79ab7e06bacd1ce6e126dc1b5907e5e5d0bf
23eliu <23eliu@athenian.org>
2021-01-19 20:19:07 +0000

    finished Pascal

--- src/main/java/Pascal.java
+++ src/main/java/Pascal.java
@@ -6,8 +6,20 @@
  */
 public class Pascal
 {
-    public static int[][] pascalTriangle(int n) {
-        // YOUR CODE HERE
-        return null;
+    public static int[][] pascalTriangle(int n) {   
+        int[][] mat = new int[n][n];
+        mat[0][0] = 1;
+
+        for (int row = 1; row < mat.length; row++) {
+            for (int col = 0; col < mat[0].length; col++) {
+                if (col == 0) {
+                    mat[row][col] = 1;
+                }
+                else {
+                    mat[row][col] = mat[row-1][col] + mat[row-1][col-1];
+                }
+            }
+        }
+        return mat;
     }
 }
